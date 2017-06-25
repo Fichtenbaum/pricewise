e8bab57c79a2d4b08aeca2f24bed3ad2b517c2ef
Olena <olena.khom@gmail.com>
2017-06-25 16:57:00 +0300

    add tearDownClass method

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,14 +1,19 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
 public class SimpleTest {
+    WebDriver driver = new FirefoxDriver();
 
     @Test
     public void navigateTest(){
-        WebDriver driver = new FirefoxDriver();
         driver.get("https://petition.president.gov.ua/");
     }
 
+    @AfterClass
+    public void tearDownClass(){
+        driver.quit();
+    }
 
 }
